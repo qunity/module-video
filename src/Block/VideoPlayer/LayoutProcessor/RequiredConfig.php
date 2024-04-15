02670fa48ba4bce6fe5d5109adec8b5c3e755f26
Rodion Kachkin <r.kachkin@mygento.com>
2024-04-15 17:42:22 +0400

    Add RequiredConfig layout processor for VideoPlayer block

---
+++ src/Block/VideoPlayer/LayoutProcessor/RequiredConfig.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * @noinspection PhpUnused
+ * @noinspection PhpPossiblePolymorphicInvocationInspection
+ */
+
+declare(strict_types=1);
+
+namespace Qunity\Video\Block\VideoPlayer\LayoutProcessor;
+
+use Magento\Framework\DataObject;
+use Magento\Framework\Stdlib\ArrayManager;
+use Qunity\Video\Api\VideoPlayer\LayoutProcessorInterface;
+
+class RequiredConfig implements LayoutProcessorInterface
+{
+    /**
+     * @param ArrayManager $arrayManager
+     */
+    public function __construct(
+        private readonly ArrayManager $arrayManager
+    ) {
+        // ...
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function process(array $jsLayout, DataObject $config): array
+    {
+        $videoId = $config->getVideoId(); // @phpstan-ignore-line
+        $videoSrc = $config->getVideoSrc(); // @phpstan-ignore-line
+
+        $path = implode('/', ['components', $videoId, 'config']);
+
+        return $this->arrayManager->merge($path, $jsLayout, [
+            'videoId' => $videoId,
+            'videoSrc' => $videoSrc,
+        ]);
+    }
+}
