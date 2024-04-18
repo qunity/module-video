72d4ae9bf951661097bac963767e8c2367eb80c1
Rodion Kachkin <r.kachkin@mygento.com>
2024-04-18 17:27:07 +0400

    Add requirejs-config.js file with video player components

---
+++ src/view/base/requirejs-config.js
@@ -0,0 +1,22 @@
+let config = {
+  map: {
+    "*": {
+      "uiYoutube": "Qunity_Video/js/youtube",
+      "uiVideo": "Qunity_Video/js/video",
+      "video/player": "Qunity_Video/js/video/player",
+      "video/uiComponent": "Qunity_Video/js/video/component/ui-component",
+      "video/uiTitleBar": "Qunity_Video/js/video/component/magento/title-bar",
+      "video/uiBigPlayButton": "Qunity_Video/js/video/component/magento/big-play-button",
+      "video/uiPosterImage": "Qunity_Video/js/video/component/magento/poster-image"
+    }
+  },
+  paths: {
+    "json": 'Qunity_Video/js/lib/requirejs/json.min',
+    "videojs": "Qunity_Video/js/lib/videojs/video.min",
+    "videojs/youtube": "Qunity_Video/js/lib/videojs/youtube.min",
+    "videojs/lang/ru": "Qunity_Video/js/lib/videojs/lang/ru.min.json",
+    "videojs/component": "Qunity_Video/js/video/component/vjs-component",
+    "videojs/component/title-bar": "Qunity_Video/js/video/component/videojs/title-bar",
+    "videojs/component/poster-image": "Qunity_Video/js/video/component/videojs/poster-image"
+  }
+}
