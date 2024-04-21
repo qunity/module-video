ffb90563b3df5c34eae25ad1b7314f488b91a666
Rodion Kachkin <r.kachkin@mygento.com>
2024-04-21 10:45:27 +0400

    Create big-play-button.js for VideoJs player

---
+++ src/view/base/web/js/video/component/videojs/big-play-button.js
@@ -0,0 +1,60 @@
+define([
+  'videojs/component',
+  'ko'
+], function (vjsComponent, ko) {
+  'use strict';
+
+  /**
+   * Big play button component for VideoJs player
+   */
+  return class BigPlayButton extends vjsComponent { // noinspection JSUnusedGlobalSymbols
+
+    /**
+     * @inheritDoc
+     */
+    _initialize() {
+      this.subElements = {};
+      this.on('click', this._onPlayPauseToggle.bind(this));
+
+      this.paused = ko.observable(null);
+      this.paused.subscribe(this._animateSubElements.bind(this));
+    }
+
+    /**
+     * Process event of toggle play/pause states
+     * @private
+     */
+    _onPlayPauseToggle() {
+      const player = this.player();
+      if (!player.isReady_) {
+        return;
+      }
+
+      const tech = player.tech(player.techName_);
+      this._callPlayPauseToggle(tech);
+    }
+
+    /**
+     * Call process of toggle play/pause states
+     * @private
+     *
+     * @param {Object} tech
+     */
+    _callPlayPauseToggle(tech) {
+      const isPaused = tech.paused();
+      isPaused ? tech.play() : tech.pause();
+
+      this.paused(isPaused);
+    }
+
+    /**
+     * Animate sub-buttons HTML elements
+     * @private
+     *
+     * @param {Boolean} paused
+     */
+    _animateSubElements(paused) {
+      this.wrapper.active(paused ? 'play' : 'pause');
+    }
+  }
+});
