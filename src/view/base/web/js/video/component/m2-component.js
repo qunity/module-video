f54a632fce3bb4b17f09ef4cd54c7c524f465a15
Rodion Kachkin <r.kachkin@mygento.com>
2024-04-27 00:08:19 +0400

    Fixes m2-component.js

---
+++ src/view/base/web/js/video/component/m2-component.js
@@ -0,0 +1,82 @@
+define([
+  'uiComponent'
+], function (uiComponent) {
+  'use strict';
+
+  /**
+   * UI component for integration into Magento Video
+   */
+  return uiComponent.extend({
+    defaults: {
+      animationClass: '_animate'
+    },
+
+    /**
+     * Component initialization
+     * @public
+     *
+     * @returns {uiComponent}
+     */
+    initialize: function () {
+      this._super();
+
+      this.initSubscriber();
+      this.initVjsComponent();
+
+      return this;
+    },
+
+    /**
+     * Initializes observable properties
+     * @public
+     *
+     * @returns {uiComponent}
+     */
+    initObservable: function () {
+      this._super();
+      this.observe(['element']);
+
+      return this;
+    },
+
+    /**
+     * Initializes subscription properties
+     * @public
+     *
+     * @returns {uiComponent}
+     */
+    initSubscriber: function () {
+      return this;
+    },
+
+    /**
+     * Initializes VideoJs component
+     * @public
+     *
+     * @returns {uiComponent}
+     */
+    initVjsComponent: function () {
+      if (this.videojsComponent) {
+        this.videojsComponent = this.videojsComponent(this);
+      }
+
+      return this;
+    },
+
+    /**
+     * Animate component elements
+     * @public
+     *
+     * @param {VoidFunction} callback
+     */
+    animate: function (callback = () => { }) {
+      const element = this.element(), fnRemoveClass = () => {
+        element.classList.remove(this.animationClass);
+        callback(this);
+      };
+
+      element.classList.add(this.animationClass);
+      element.addEventListener('animationend', fnRemoveClass, { once: true });
+    }
+  });
+});
