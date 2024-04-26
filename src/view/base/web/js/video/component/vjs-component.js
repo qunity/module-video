7aded1e0f2e7e71ceef6dd32e45656437ecfa540
Rodion Kachkin <r.kachkin@mygento.com>
2024-04-27 00:48:52 +0400

    Fixes vjs-component.js

--- src/view/base/web/js/video/component/vjs-component.js
+++ src/view/base/web/js/video/component/vjs-component.js
@@ -1,5 +1,3 @@
-// noinspection JSUnresolvedReference
-
 define([
   'videojs'
 ], function (videojs) {
@@ -8,73 +6,80 @@ define([
   /**
    * Base class of component for VideoJs player
    */
-  return class extends videojs.getComponent('Component') {
+  return function (m2Component) {
+    const vjsComponent = class extends videojs.getComponent('Component') {
 
-    /**
-     * Component constructor
-     * @public
-     *
-     * @param {Object} player
-     * @param {Object} options
-     */
-    constructor(player, options = {}) {
-      super(player, options);
-      this._initialize(player, options);
-    }
-
-    /**
-     * Get component wrapper
-     * @public
-     *
-     * @returns {Object}
-     */
-    get wrapper() {
-      const value = this.constructor.wrapper;
-
-      if (value === null || typeof value !== 'object') {
-        throw new Error('Class wrapper has not been installed');
+      /**
+       * Component constructor
+       * @public
+       *
+       * @param {Object} player
+       * @param {Object} options
+       */
+      constructor(player, options = {}) {
+        super(player, options);
+        this._initialize(player, options);
       }
 
-      return this.constructor.wrapper;
-    }
+      /**
+       * Get component wrapper
+       * @public
+       *
+       * @returns {m2Component}
+       */
+      get wrapper() {
+        const value = this.constructor.wrapper;
 
-    /**
-     * Set component wrapper
-     * @public
-     *
-     * @param {Object} value
-     */
-    set wrapper(value) {
-      if (value === null || typeof value !== 'object') {
-        throw new Error('Inputted wrapper value is not an object');
+        if (value === null || typeof value !== 'object') {
+          throw new Error('Class wrapper has not been installed');
+        }
+
+        return this.constructor.wrapper;
       }
 
-      if (this.constructor.wrapper) {
-        throw new Error('Class wrapper is already installed');
+      /**
+       * Set component wrapper
+       * @public
+       *
+       * @param {m2Component} value
+       */
+      set wrapper(value) {
+        if (value === null || typeof value !== 'object') {
+          throw new Error('Inputted wrapper value is not an object');
+        }
+
+        if (this.constructor.wrapper) {
+          throw new Error('Class wrapper is already installed');
+        }
+
+        this.constructor.wrapper = value;
       }
 
-      this.constructor.wrapper = value;
-    }
+      /**
+       * Create HTML element
+       * @public
+       *
+       * @returns {HTMLElement}
+       */
+      createEl() {
+        // noinspection JSUnresolvedReference
+        return this.wrapper.element();
+      }
 
-    /**
-     * Create HTML element
-     * @public
-     *
-     * @returns {HTMLElement}
-     */
-    createEl() {
-      return this.wrapper.element();
-    }
+      /**
+       * Component initialization
+       * @private
+       *
+       * @param {Object} player
+       * @param {Object} options
+       */
+      _initialize(player, options) {
+        // ...
+      }
+    };
 
-    /**
-     * Component initialization
-     * @private
-     *
-     * @param {Object} player
-     * @param {Object} options
-     */
-    _initialize(player, options) {
-      // ...
-    }
+    vjsComponent.wrapper = m2Component;
+
+    return vjsComponent;
   };
 });
