81bf4437f1301ca23d0aa0456def03af03f4c953
Rodion Kachkin <r.kachkin@mygento.com>
2024-04-26 23:40:25 +0400

    Fix requirejs-config configuration #2

--- src/view/base/requirejs-config.js
+++ src/view/base/requirejs-config.js
@@ -7,21 +7,21 @@ let config = {
       "video/preview": "Qunity_Video/js/video/preview",
       "video/uiAbstract": "Qunity_Video/js/video/ui-abstract",
       "video/m2Component": "Qunity_Video/js/video/component/m2-component",
+      "video/vjsComponent": "Qunity_Video/js/video/component/vjs-component",
       "video/m2TitleBar": "Qunity_Video/js/video/component/magento/title-bar",
+      "video/vjsTitleBar": "Qunity_Video/js/video/component/videojs/title-bar",
       "video/m2BigPlayButton": "Qunity_Video/js/video/component/magento/big-play-button",
+      "video/vjsBigPlayButton": "Qunity_Video/js/video/component/videojs/big-play-button",
       "video/m2PosterImage": "Qunity_Video/js/video/component/magento/poster-image",
-      "video/m2ErrorInfo": "Qunity_Video/js/video/component/magento/error-info"
+      "video/vjsPosterImage": "Qunity_Video/js/video/component/videojs/poster-image",
+      "video/m2ErrorInfo": "Qunity_Video/js/video/component/magento/error-info",
+      "video/vjsErrorInfo": "Qunity_Video/js/video/component/videojs/error-info"
     }
   },
   paths: {
     "json": 'Qunity_Video/js/lib/requirejs/json.min',
     "videojs": "Qunity_Video/js/lib/videojs/video.min",
     "videojs/youtube": "Qunity_Video/js/lib/videojs/youtube.min",
-    "videojs/lang/ru": "Qunity_Video/js/lib/videojs/lang/ru.min.json",
-    "videojs/vjsComponent": "Qunity_Video/js/video/component/vjs-component",
-    "videojs/vjsTitleBar": "Qunity_Video/js/video/component/videojs/title-bar",
-    "videojs/vjsBigPlayButton": "Qunity_Video/js/video/component/videojs/big-play-button",
-    "videojs/vjsPosterImage": "Qunity_Video/js/video/component/videojs/poster-image",
-    "videojs/vjsErrorInfo": "Qunity_Video/js/video/component/videojs/error-info"
+    "videojs/lang/ru": "Qunity_Video/js/lib/videojs/lang/ru.min.json"
   }
 }
