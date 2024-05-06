693326bdba4e457dad753d412602ce11f19109ec
Rodion Kachkin <r.kachkin@mygento.com>
2024-05-06 21:47:06 +0400

    Create implementation Component class

---
+++ src/Model/Data/VideoPlayer/Config/Component.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Qunity\Video\Model\Data\VideoPlayer\Config;
+
+use Magento\Framework\DataObject;
+use Qunity\Video\Api\Data\VideoPlayer\Config\ComponentInterface;
+
+class Component extends DataObject implements ComponentInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function getCode(): ?string
+    {
+        return $this->hasData(self::CODE)
+            ? (string) $this->getData(self::CODE) : null;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setCode(string $code): ComponentInterface
+    {
+        return $this->setData(self::CODE, $code);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getAlias(): ?string
+    {
+        return $this->hasData(self::ALIAS)
+            ? (string) $this->getData(self::ALIAS) : null;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setAlias(string $alias): ComponentInterface
+    {
+        return $this->setData(self::ALIAS, $alias);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getPath(): ?string
+    {
+        return $this->hasData(self::PATH)
+            ? (string) $this->getData(self::PATH) : null;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setPath(string $path): ComponentInterface
+    {
+        return $this->setData(self::PATH, $path);
+    }
+}
