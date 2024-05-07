336f2c7933506a9228589afa46761acd85f52887
Rodion Kachkin <r.kachkin@mygento.com>
2024-05-08 00:22:28 +0400

    Move components to view directory

--- src/view/base/requirejs-config.js
+++ src/view/base/requirejs-config.js
@@ -1,21 +1,21 @@
 let config = {
   map: {
     "*": {
-      "uiYouTube": "Qunity_Video/js/youtube",
-      "uiVideo": "Qunity_Video/js/video",
-      "video/player": "Qunity_Video/js/video/player",
-      "video/preview": "Qunity_Video/js/video/preview",
-      "video/uiAbstract": "Qunity_Video/js/video/ui-abstract",
-      "video/m2Component": "Qunity_Video/js/video/component/m2-component",
-      "video/vjsComponent": "Qunity_Video/js/video/component/vjs-component",
-      "video/m2TopBar": "Qunity_Video/js/video/component/magento/top-bar",
-      "video/vjsTopBar": "Qunity_Video/js/video/component/videojs/top-bar",
-      "video/m2BigPlayButton": "Qunity_Video/js/video/component/magento/big-play-button",
-      "video/vjsBigPlayButton": "Qunity_Video/js/video/component/videojs/big-play-button",
-      "video/m2PosterImage": "Qunity_Video/js/video/component/magento/poster-image",
-      "video/vjsPosterImage": "Qunity_Video/js/video/component/videojs/poster-image",
-      "video/m2ErrorInfo": "Qunity_Video/js/video/component/magento/error-info",
-      "video/vjsErrorInfo": "Qunity_Video/js/video/component/videojs/error-info"
+      "uiYouTube": "Qunity_Video/js/view/youtube",
+      "uiVideo": "Qunity_Video/js/view/video",
+      "video/player": "Qunity_Video/js/view/video/player",
+      "video/preview": "Qunity_Video/js/view/video/preview",
+      "video/uiAbstract": "Qunity_Video/js/view/video/ui-abstract",
+      "video/m2Component": "Qunity_Video/js/view/video/component/m2-component",
+      "video/vjsComponent": "Qunity_Video/js/view/video/component/vjs-component",
+      "video/m2TopBar": "Qunity_Video/js/view/video/component/magento/top-bar",
+      "video/vjsTopBar": "Qunity_Video/js/view/video/component/videojs/top-bar",
+      "video/m2BigPlayButton": "Qunity_Video/js/view/video/component/magento/big-play-button",
+      "video/vjsBigPlayButton": "Qunity_Video/js/view/video/component/videojs/big-play-button",
+      "video/m2PosterImage": "Qunity_Video/js/view/video/component/magento/poster-image",
+      "video/vjsPosterImage": "Qunity_Video/js/view/video/component/videojs/poster-image",
+      "video/m2ErrorInfo": "Qunity_Video/js/view/video/component/magento/error-info",
+      "video/vjsErrorInfo": "Qunity_Video/js/view/video/component/videojs/error-info"
     }
   },
   paths: {
