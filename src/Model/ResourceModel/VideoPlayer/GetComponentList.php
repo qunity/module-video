4785d6177c77bc30726eb1c20f33b3901396f863
Rodion Kachkin <r.kachkin@mygento.com>
2024-05-07 10:09:48 +0400

    Create GetComponentList.php class

---
+++ src/Model/ResourceModel/VideoPlayer/GetComponentList.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Qunity\Video\Model\ResourceModel\VideoPlayer;
+
+use Qunity\Video\Api\Data\VideoPlayer\Config\ComponentInterface;
+use Qunity\Video\Api\Data\VideoPlayer\Config\ComponentInterfaceFactory;
+
+class GetComponentList
+{
+    /**
+     * @var ComponentInterface[]
+     */
+    private array $items;
+
+    /**
+     * @param ComponentInterfaceFactory $componentFactory
+     * @param array $data
+     */
+    public function __construct(
+        private readonly ComponentInterfaceFactory $componentFactory,
+        private readonly array $data = []
+    ) {
+        // ...
+    }
+
+    /**
+     * Get all registered JS Components for Video Player
+     *
+     * @return ComponentInterface[]
+     */
+    public function execute(): array
+    {
+        if (!empty($this->items)) {
+            return $this->items;
+        }
+
+        foreach ($this->data as $code => $item) {
+            $item[ComponentInterface::CODE] = $code;
+            $this->items[$code] = $this->componentFactory->create(['data'=> $item]);
+        }
+
+        return $this->items;
+    }
+}
