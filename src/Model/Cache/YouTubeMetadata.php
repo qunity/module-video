117644954e9cbb2663b64a0d324a8dea9e0fb8ae
Rodion Kachkin <r.kachkin@mygento.com>
2024-05-26 13:59:18 +0400

    Create new cache type YouTubeMetadata

---
+++ src/Model/Cache/YouTubeMetadata.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Qunity\Video\Model\Cache;
+
+use Magento\Framework\App\Cache\Type\FrontendPool;
+use Magento\Framework\Cache\Frontend\Decorator\TagScope;
+
+class YouTubeMetadata extends TagScope
+{
+    /**
+     * Cache type code unique among all cache types
+     */
+    public const TYPE_IDENTIFIER = 'youtube_metadata';
+
+    /**
+     * The tag name that limits the cache cleaning scope within a particular tag
+     */
+    public const CACHE_TAG = 'YOUTUBE_METADATA';
+
+    /**
+     * @param FrontendPool $cacheFrontendPool
+     */
+    public function __construct(FrontendPool $cacheFrontendPool)
+    {
+        parent::__construct($cacheFrontendPool->get(self::TYPE_IDENTIFIER), self::CACHE_TAG);
+    }
+}
